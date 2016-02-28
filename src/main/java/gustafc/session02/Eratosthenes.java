ff59cf9425f60fda6ae072b72cb904d7682801ff
Gustaf Cele <gustafc@gmail.com>
2016-02-28 20:12:56 +0100

    Sieve of Eratosthenes

---
+++ src/main/java/gustafc/session02/Eratosthenes.java
@@ -0,0 +1,41 @@
+package gustafc.session02;
+
+import rx.Observable;
+
+public class Eratosthenes {
+
+    public static final int LAST_SIGNED_32_BIT_PRIME = 2147483647;
+
+    static class Sifter {
+
+        private final Sifter previous;
+        private final int prime;
+
+        Sifter(Sifter previous, int n) {
+            this.previous = previous;
+            this.prime = n;
+        }
+
+        Sifter next() {
+            int candidate = prime;
+            while (!isPrime(++candidate))
+                ;
+            return new Sifter(this, candidate);
+        }
+
+        private boolean isPrime(int candidate) {
+            for (Sifter current = this; current != null; current = current.previous) {
+                if (candidate % current.prime == 0) return false;
+            }
+            return true;
+        }
+    }
+
+    public static Observable<Integer> primes(){
+        return Observable.just("whatever").repeat()
+                .scan(new Sifter(null, 2), (sifter, ignore) -> sifter.next())
+                .map(sifter -> sifter.prime)
+                .takeUntil(n -> n == LAST_SIGNED_32_BIT_PRIME);
+    }
+
+}
---
+++ src/test/java/gustafc/session02/EratosthenesTest.java
@@ -0,0 +1,16 @@
+package gustafc.session02;
+
+import org.junit.Test;
+
+import static gustafc.testutil.ObservableMatchers.emitsValues;
+import static org.junit.Assert.assertThat;
+
+public class EratosthenesTest {
+
+    @Test
+    public void testSieve() {
+        assertThat(Eratosthenes.primes().take(20),
+                emitsValues(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71));
+    }
+
+}
