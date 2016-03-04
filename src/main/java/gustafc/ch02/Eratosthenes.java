290872282e95212646000139f4c26d61f6cd2edd
Gustaf Cele <gustafc@gmail.com>
2016-03-04 23:34:13 +0100

    Verify we actially test the right thing

--- src/main/java/gustafc/ch02/Eratosthenes.java
+++ src/main/java/gustafc/ch02/Eratosthenes.java
@@ -2,6 +2,9 @@ package gustafc.ch02;
 
 import rx.Observable;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 public class Eratosthenes {
 
     public static final int LAST_SIGNED_32_BIT_PRIME = 2147483647;
@@ -31,11 +34,25 @@ public class Eratosthenes {
         }
     }
 
-    public static Observable<Integer> primes(){
+    public static Observable<Integer> primes() {
         return Observable.just("whatever").repeat()
                 .scan(new Sifter(null, 2), (sifter, ignore) -> sifter.next())
                 .map(sifter -> sifter.prime)
                 .takeUntil(n -> n == LAST_SIGNED_32_BIT_PRIME);
     }
+
+    public static Observable<Integer> nonFp() {
+        Collection<Integer> primes = new HashSet<>();
+        return Observable.just(1).repeat().scan(2, (a, b) -> a + b).filter(integer -> {
+            boolean isPrime = isPrime(integer, primes);
+            if (isPrime) primes.add(integer);
+            return isPrime;
+        });
+    }
+
+    private static boolean isPrime(Integer integer, Collection<Integer> primes) {
+        return !primes.stream().anyMatch(prime -> integer % prime == 0);
+    }
+
 
 }
--- src/test/java/gustafc/ch02/EratosthenesTest.java
+++ src/test/java/gustafc/ch02/EratosthenesTest.java
@@ -1,6 +1,8 @@
 package gustafc.ch02;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
+import rx.Observable;
 
 import static gustafc.testutil.ObservableMatchers.emitsValues;
 import static org.junit.Assert.assertThat;
@@ -9,8 +11,16 @@ public class EratosthenesTest {
 
     @Test
     public void testSieve() {
-        assertThat(Eratosthenes.primes().take(20),
-                emitsValues(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71));
+        assertThat(Eratosthenes.primes().take(25), emits25FirstPrimes());
+    }
+
+    @Test
+    public void testNonFp() {
+        assertThat(Eratosthenes.nonFp().take(25), emits25FirstPrimes());
+    }
+
+    private Matcher<Observable<? extends Integer>> emits25FirstPrimes() {
+        return emitsValues(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97);
     }
 
 }
