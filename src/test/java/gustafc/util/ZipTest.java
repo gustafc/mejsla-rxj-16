034cf2655d137b9a50f1585227050a690db77e39
Gustaf Cele <gustafc@gmail.com>
2016-05-04 08:48:52 +0200

    Add test for double subscribe

--- src/test/java/gustafc/util/ZipTest.java
+++ src/test/java/gustafc/util/ZipTest.java
@@ -3,19 +3,24 @@ package gustafc.util;
 import org.junit.Test;
 import rx.Observable;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static gustafc.testutil.ObservableMatchers.emitsNothing;
 import static gustafc.testutil.ObservableMatchers.emitsValues;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class ZipTest {
 
     @Test
-    public void shouldWorkForEmptySequence(){
+    public void shouldWorkForEmptySequence() {
         assertThat(Zip.withIndex(Observable.empty()), emitsNothing());
     }
 
     @Test
-    public void shouldGiveCorrectIndices(){
+    public void shouldGiveCorrectIndices() {
         assertThat(Zip.withIndex(Observable.just("apelsin", "banan", "citron", "durian")),
                 emitsValues(
                         new Zip.Indexed<>(0, "apelsin"),
@@ -23,6 +28,27 @@ public class ZipTest {
                         new Zip.Indexed<>(2, "citron"),
                         new Zip.Indexed<>(3, "durian")
                 ));
+    }
+
+    @Test
+    public void shouldGiveCorrectIndicesWithDoubleSubscribe() {
+        Observable<Zip.Indexed<String>> zipped = Zip.withIndex(Observable.just("apelsin", "banan", "citron", "durian"));
+        List<Zip.Indexed<String>> output = new ArrayList<>();
+        zipped.toBlocking().forEach(output::add);
+        zipped.cache().toBlocking().forEach(output::add);
+        assertEquals(
+                Arrays.asList(
+                        new Zip.Indexed<>(0, "apelsin"),
+                        new Zip.Indexed<>(1, "banan"),
+                        new Zip.Indexed<>(2, "citron"),
+                        new Zip.Indexed<>(3, "durian"),
+                        new Zip.Indexed<>(0, "apelsin"),
+                        new Zip.Indexed<>(1, "banan"),
+                        new Zip.Indexed<>(2, "citron"),
+                        new Zip.Indexed<>(3, "durian")
+                ),
+                output
+        );
     }
 
 }
